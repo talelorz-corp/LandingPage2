bc0a2ef8452565473738b147267641575991ff2c
Taegu Han <hantaegu96@naver.com>
2023-03-14 14:43:50 +0900

    3/14 work start

--- components/Postcard.tsx
+++ components/Postcard.tsx
@@ -1,32 +1,38 @@
-import { useRef, useEffect} from "react"
-export function Postcard(props: 
-    {post: Post & {likes:number, hashtags?:string[], category?:string}}) 
-  {
-
-    return (
-      <>
-        <div key={props.post.postId} style={
-            {
-                width: "500px", height:"auto", 
-                backgroundColor: "#EEEEEE",
-                padding:"20px 20px 20px 20px"
-            }
-        }>
-          <p style={{fontFamily:"sans-serif", fontSize:"20px"}}>
-            {props.post.userId + " _" + props.post.postId}
+import { useRef, useEffect } from "react";
+export function Postcard(props: {
+  post: Post & { likes: number; hashtags?: string[]; category?: string };
+}) {
+  return (
+    <>
+      <div
+        key={props.post.postId}
+        style={{
+          width: "500px",
+          height: "auto",
+          backgroundColor: "#EEEEEE",
+          padding: "20px 20px 20px 20px",
+        }}
+      >
+        <p style={{ fontFamily: "sans-serif", fontSize: "20px" }}>
+          {props.post.authorId + " _" + props.post.postId}
+        </p>
+        <center>
+          <p
+            style={{
+              width: "480px",
+              height: "460px",
+              backgroundColor: "#FFFFFF",
+              textAlign: "left",
+              padding: "10px 10px 10px 10px",
+            }}
+          >
+            {props.post.content}
           </p>
-          <center>
-            <p style={{width: "480px", height: "460px", backgroundColor : "#FFFFFF", textAlign:"left",
-            padding: "10px 10px 10px 10px"}}>
-                {props.post.content}
-            </p>
-          </center>
-          <p style={{fontFamily:"sans-serif", fontSize:"20px"}}>
-            Likes: {props.post.likes}
-          </p>
-        </div>
-      </>
-  
-    )
-  }
-  
+        </center>
+        <p style={{ fontFamily: "sans-serif", fontSize: "20px" }}>
+          Likes: {props.post.likes}
+        </p>
+      </div>
+    </>
+  );
+}
--- pages/makeTale.tsx
+++ pages/makeTale.tsx
@@ -1,10 +1,43 @@
 import Avatar from "@/components/Avatar";
 import Navigation from "@/components/Navigation";
+import { GenerateTaleRequestData } from "@/server_src/models/models";
 import Head from "next/head";
 import React, { useState } from "react";
+import { MouseEvent } from "react";
 
 function makeTale() {
   const [input, setInput] = useState<string>("");
+  const [output, setOutput] = useState<string>("");
+
+  const generateTale = async () => {
+    const taleInfo: GenerateTaleRequestData = {
+      input: input,
+    };
+    console.log("start fetch");
+    const result = await fetch(
+      "http://localhost:3000/api/posts/crud/generate",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(taleInfo),
+        method: "POST",
+      }
+    );
+    console.log("finish fetch");
+    const json = await result.json();
+    const novel = json["result"];
+    setOutput(novel);
+  };
+
+  const handleSubmit = (
+    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    generateTale();
+  };
+
   return (
     <div className="my-7 mx-auto max-w-5xl">
       <Head>
@@ -32,11 +65,21 @@ function makeTale() {
             <button
               disabled={!input}
               className="rounded-full bg-amber-700 px-5 py-2 font-bold text-white disabled:opacity-40"
+              onClick={handleSubmit}
             >
               테일 생성
             </button>
           </form>
+          <div className="w-auto min-w-[75%] max-w-min mx-auto space-y-6 flex flex-col items-stretch mb-8">
+            <textarea
+              value={output}
+              onChange={(e) => e.preventDefault()}
+              placeholder="테일 생성 결과"
+              className="border-2 rounded border-gray-600 p-1 whitespace-pre-line"
+            ></textarea>
+          </div>
         </div>
+
         <div className="sticky bottom-0 z-50 flex-1 justify-center items-center">
           <Navigation />
         </div>
